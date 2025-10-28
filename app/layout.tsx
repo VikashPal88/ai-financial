f24491c74b53b7cfee27cb172ef8535a5fb18043
Vikash Pal <141484082+VikashPal88@users.noreply.github.com>
2025-10-28 20:44:26 +0530

    remove footer
    Comment out unused div elements in layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,14 @@ export default function RootLayout({
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
 
-          <div className="relative flex h-[15rem] w-full items-center justify-center bg-black">
+          {/* <div className="relative flex h-[15rem] w-full items-center justify-center bg-black">
             <div
               className={cn(
                 "absolute inset-0",
                 "[background-size:40px_40px]",
                 "[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
               )}
-            />
+            /> */}
             {/* <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] bg-black" />
             <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
               BUILD BY SYNTAX SAMURAI
